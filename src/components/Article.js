29e27934908c90c50aaa1fb0e7239bf8792c7004
Kamente <justinm48000@gmail.com>
2023-07-27 23:50:19 +0300

    done

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function Article({ title, date, preview }) {
+    const incaseDate = date || "January 1,1970"
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{incaseDate}</small>
             <p>{preview}</p>
         </article>
     );
